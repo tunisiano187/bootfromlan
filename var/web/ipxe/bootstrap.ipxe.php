1a15998cabb53973a9063520b1099c99d40dd7e6
tunisiano187 <tunisiano187@users.noreply.github.com>
2015-08-26 05:49:50 +0200

    Update bootstrap.ipxe.php adding variables
    Update bootstrap.ipxe.php adding variables from the ipxe

--- var/web/ipxe/bootstrap.ipxe.php
+++ var/web/ipxe/bootstrap.ipxe.php
@@ -7,5 +7,8 @@ set menu-url ${boot-url}/menu.ipxe
 
 chain --replace --autofree ${menu-url} ||
 <?php
-	
+	$asset=$_GET['asset'];
+	$mac=$_GET['mac'];
+	$serial=$_GET['serial'];
+	$uuid=$_GET['uuid'];
 ?>
